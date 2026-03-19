1a3d2001d835e966c640b6161c4401648863eeab
12HMan12 <pgenter@web.de>
2026-03-19 09:15:05 +0100

    Add the Netherlands flag image to the assets directory

--- src/lib/slider.ts
+++ src/lib/slider.ts
@@ -9,7 +9,7 @@ const stops = Object.entries(COUNTRIES).map(([code, data]) => ({
 
 const track = document.getElementById("slider-track")! as HTMLElement;
 const handle = document.querySelector(".slider-handle") as HTMLElement;
-const tooltip = document.getElementById("slider-tooltip") as HTMLElement;
+// const tooltip = document.getElementById("slider-tooltip") as HTMLElement;
 const progress = document.querySelector(".slider-progress") as HTMLElement;
 
 let dragging = false;
@@ -18,14 +18,24 @@ let dragPercentage = 0;
 
 stops.forEach((_stop, index) => {
   const el = document.createElement("div");
-  // const img = document.createElement("img");
+  const tooltip = document.createElement("div");
+  const tooltipText = document.createElement("span");
+  const tooltipImg = document.createElement("img");
+  tooltip.className = "slider-tooltip";
+  tooltip.id = `tooltip-${_stop.country}`;
+  tooltipText.textContent = _stop.label;
+  tooltip.appendChild(tooltipText);
+  tooltipImg.src = COUNTRIES[_stop.country].img;
+  tooltip.appendChild(tooltipImg);
+  // tooltip.textContent = _stop.label;
+  el.appendChild(tooltip);
   // img.src = COUNTRIES[_stop.country].img;
   // img.alt = _stop.label;
   // el.appendChild(img);
 
   el.className = "slider-stop";
   el.style.left = `${(index / (stops.length - 1)) * 100}%`;
-  el.style.backgroundImage = `url(${COUNTRIES[_stop.country].img})`;
+  // el.style.backgroundImage = `url(${COUNTRIES[_stop.country].img})`;
   track.appendChild(el);
 });
 
@@ -47,7 +57,8 @@ function snapTo(index: number) {
   updateProgress(percent);
   // tooltip.textContent = stops[index].label;
   setActiveStop(index);
-  tooltip.textContent = stops[index].label;
+  previewStop(percent);
+  // tooltip.textContent = stops[index].label;
 
   // kleines haptic feedback
   if (navigator.vibrate) {
@@ -61,13 +72,17 @@ function nearestStop(percent: number) {
   return Math.round(percent * (stops.length - 1));
 }
 
-function showTooltip() {
-  tooltip.style.opacity = "1";
-}
+// function showTooltip(percent: number) {
+//   const index = Math.round(percent * (stops.length - 1));
+//   document.getElementById(`tooltip-${stops[index].country}`)?.classList.add("active");
+//   // tooltip.style.opacity = "1";
+// }
 
-function hideTooltip() {
-  tooltip.style.opacity = "0";
-}
+// function hideTooltip(percent: number) {
+//   const index = Math.round(percent * (stops.length - 1));
+//   document.getElementById(`tooltip-${stops[index].country}`)?.classList.remove("active");
+//   // tooltip.style.opacity = "0";
+// }
 
 function updateSlider(percent: number) {
   handle.style.left = `${percent * 100}%`;
@@ -81,8 +96,10 @@ function updateProgress(percent: number) {
 
 function previewStop(percent: number) {
   const index = Math.round(percent * (stops.length - 1));
+  document.querySelectorAll(".slider-tooltip").forEach((stop) => stop.classList.remove("active"));
+  document.getElementById(`tooltip-${stops[index].country}`)?.classList.add("active");
 
-  tooltip.textContent = stops[index].label;
+  // tooltip.textContent = stops[index].label;
 }
 
 function setActiveStop(index: number) {
@@ -117,7 +134,7 @@ track.addEventListener("click", (e: MouseEvent) => {
   dragPercentage = percent;
 
   updateSlider(percent);     // Handle + Progress sofort bewegen
-  previewStop(percent);      // Tooltip aktualisieren
+  // previewStop(percent);      // Tooltip aktualisieren
 
   const index = nearestStop(percent);
   snapTo(index);             // Snap + Map wechseln
@@ -151,7 +168,7 @@ handle.addEventListener("mousedown", () => {
   dragging = true;
   document.body.style.userSelect = "none";
   document.body.style.cursor = "grabbing";
-  showTooltip();
+  // showTooltip(dragPercentage);
 });
 
 document.addEventListener("mousemove", (e) => {
@@ -182,14 +199,14 @@ document.addEventListener("mouseup", () => {
 
   // snapTo(index);
   snapTo(nearestStop(dragPercentage));
-  hideTooltip();
+  // hideTooltip(dragPercentage);
 });
 
 handle.addEventListener("touchstart", () => {
   dragging = true;
   document.body.style.userSelect = "none";
   document.body.style.cursor = "grabbing";
-  showTooltip();
+  // showTooltip(dragPercentage);
 });
 
 document.addEventListener("touchmove", (e) => {
@@ -200,7 +217,7 @@ document.addEventListener("touchmove", (e) => {
   dragPercentage = percent;
 
   updateSlider(percent);
-  previewStop(percent);
+  // previewStop(percent);
   // dragPercentage = magneticPercent(getPercentFromEvent(e));
 
   // // handle.style.left = `${percent * 100}%`;
