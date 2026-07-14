4c7eff29ad66ecf99b1d4da2ebdba868f5514834
12HMan12 <pgenter@web.de>
2026-07-14 22:03:01 +0200

    refactor: update Vite config to conditionally enable HTTPS for local server

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vite'
 import fs from 'fs'
 
-export default defineConfig({
-  server: {
-    port: 5173,
-    open: false,
-    host: true,
-    https: {
-      key: fs.readFileSync('./cert/localhost-key.pem'),
-      cert: fs.readFileSync('./cert/localhost.pem'),
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      server: {
+        port: 5173,
+        open: false,
+        host: true,
+        ...(fs.existsSync('./cert/localhost-key.pem') &&
+        fs.existsSync('./cert/localhost.pem')
+          ? {
+              https: {
+                key: fs.readFileSync('./cert/localhost-key.pem'),
+                cert: fs.readFileSync('./cert/localhost.pem'),
+              },
+            }
+          : {}),
+      },
     }
   }
+
+  return {}
 })
