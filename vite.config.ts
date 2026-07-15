909bee44f10557abf9752b112014efdca5c26ed0
12HMan12 <pgenter@web.de>
2026-07-15 11:57:45 +0200

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import fs from 'fs'
 
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      server: {
-        port: 5173,
-        open: false,
-        host: true,
-        ...(fs.existsSync('./cert/localhost-key.pem') &&
-        fs.existsSync('./cert/localhost.pem')
-          ? {
-              https: {
-                key: fs.readFileSync('./cert/localhost-key.pem'),
-                cert: fs.readFileSync('./cert/localhost.pem'),
-              },
-            }
-          : {}),
+export default defineConfig(({ command }) => ({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        map: resolve(__dirname, 'map.html'),
       },
-    }
-  }
+    },
+  },
 
-  return {}
-})
+  ...(command === 'serve'
+    ? {
+        server: {
+          port: 5173,
+          open: false,
+          host: true,
+          https: {
+            key: fs.readFileSync('./cert/localhost-key.pem'),
+            cert: fs.readFileSync('./cert/localhost.pem'),
+          },
+        },
+      }
+    : {}),
+}))
